ff462cc2055f941bde58251e8066625d4b7f585b
Tiago Sanona <tsanona@gmail.com>
2026-05-22 22:41:22 +0200

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Qobine config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs;
+      {
+        devShell = mkShell {
+          nativeBuildInputs = [ pkg-config sqlx-cli just ];
+          buildInputs = [
+            # qobuz-player + qobuz-player-connect + qobuz-player-rfid + qobuz-player-web (base libs)
+            alsa-lib
+            openssl
+            # qobuz-player-gtk (+ base libs)
+            libadwaita
+            webkitgtk_6_0
+          ];
+          ALSA_PLUGIN_DIR="${pipewire}/lib/alsa-lib";
+        };
+      }
+    );
+}
